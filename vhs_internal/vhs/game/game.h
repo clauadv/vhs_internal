96277ea8408525a4834d41677046c82582de0b19
clau <clau.burrito@gmail.com>
2022-06-26 18:25:53 +0300

    .

--- vhs_internal/vhs/game/game.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace vhs::game {
-	class a_base_char : public ue4::game_framework::a_pawn {
-	public:
-		char pad_0001[0x1110]; // 0x288(0x1110)
-		void* active_doll_minion; // 0x1398(0x08)
-	};
-}
